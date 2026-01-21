3d850c174a115a48d6029451be1648f20d0905fb
Yash-Domain <yashpawde10@gmail.com>
2026-01-21 23:18:22 +0530

    backend_complete :)

--- TechLingo-backend/src/app.js
+++ TechLingo-backend/src/app.js
@@ -19,14 +19,14 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok", message: "TechLingo API running" });
 });
 
-import debugRoutes from "./routes/debug.routes.js";
+
 
 // routes
 app.use("/api/day", dayRoutes);
 app.use("/api/progress", progressRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.use("/api/debug", debugRoutes);
+
 app.use("/api/auth", authRoutes);
 
 
